b08ed20243cda4de700296f91e6afc8aa14b3a6b
Mike Sperber <sperber@deinprogramm.de>
2020-04-21 14:51:41 +0200

    Update

--- racket/tag2-schleifen.rkt
+++ racket/tag2-schleifen.rkt
@@ -1,3 +1,16 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname tag2-schleifen) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Liste umdrehen
+(: rev ((list-of %a) -> (list-of %a)))
+
+(check-expect (rev (list 1 2 3 4))
+              (list 4 3 2 1))
+
+(define rev
+  (lambda (list)
+    (cond
+      ((empty? list) ...)
+      ((cons? list)
+       (first list)
+       (rev (rest list))))))
