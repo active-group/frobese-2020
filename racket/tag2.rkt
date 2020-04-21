2be44efb30535d5f2ab01145d9c6d99cab45dfcb
Mike Sperber <sperber@deinprogramm.de>
2020-04-21 10:59:46 +0200

    Update

--- racket/tag2.rkt
+++ racket/tag2.rkt
@@ -1,6 +1,119 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname tag2) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Ein Gürteltier hat folgende Eigenschaften
+; - lebendig oder tot
+; - Gewicht in g
+
+(define-record-functions dillo
+  make-dillo
+  dillo?
+  (dillo-alive? boolean)
+  (dillo-weight natural))
+
+(: make-dillo (boolean natural -> dillo))
+(: dillo-alive? (dillo -> boolean))
+(: dillo-weight (dillo -> natural))
+
+(: dillo? (any -> boolean))
+
+
+(define dillo1 (make-dillo #t 20000)) ; Gürteltier, lebendig, 20 kg
+(define dillo2 (make-dillo #f 15000)) ; Gürteltier, tot, 15 kg
+
+; Überfahre ein Gürteltier!
+(: run-over-dillo (dillo -> dillo))
+(check-expect (run-over-dillo dillo1)
+              (make-dillo #f 20000))
+(check-expect (run-over-dillo dillo2)
+              dillo2)
+
+#;(define run-over-dillo
+  (lambda (dillo)
+    (if (dillo-alive? dillo)
+        (make-dillo #f (dillo-weight))
+        dillo)))
+
+(define run-over-dillo
+  (lambda (dillo)
+    (make-dillo #f (dillo-weight dillo))))
+
+;;; ÜBUNG Weiteres Tier: Gewicht und Funktion die es überfährt
+
+; Ein Papagei hat folgende Eigenschaften
+; - Gewicht in Gramm
+; - Einen Satz, den er sagt
+
+(define-record-functions parrot
+  make-parrot
+  parrot?
+  (parrot-weight natural)
+  (parrot-sentence string))
+
+(define parrot1
+  (make-parrot 2000 "Mein Schatz!!"))  ; Piratenpapagei, 2 kg
+
+(define parrot2
+  (make-parrot 4000 "Ich grüße Sie!")) ; höflicher Papagei, 4 kg
+
+; Überfahre einen Papagei
+(: run-over-parrot (parrot -> parrot))
+(check-expect (run-over-parrot parrot1)
+              (make-parrot 2000 ""))
+(check-expect (run-over-parrot parrot2)
+              (make-parrot 4000 ""))
+
+(define run-over-parrot
+  (lambda (parrot)
+    (make-parrot (parrot-weight parrot) "")))
+
+;;; nicht getestet!!!
+(define parrot-alive?
+  (lambda (parrot)
+    (not (string=? "" (parrot-sentence parrot)))))
+
+
+;;; GEMISCHTEN DATEN
+; Wir wollen Tiere überfahren!
+
+; Ein Tier ist eins der folgenden
+; - Gürteltier
+; - Papagei
+; "Ist eins der folgenden" -> gemischte Daten
+
+(define animal
+  (signature (mixed dillo parrot)))
+
+; Wir wollen Tiere überfahren!
+; Überfährt ein Tier
+(: run-over-animal (animal -> animal))
+(check-expect (run-over-animal dillo1)
+              (make-dillo #f 20000))
+(check-expect (run-over-animal parrot1)
+              (make-parrot 2000 ""))
+(check-expect (run-over-animal dillo1)
+              (run-over-dillo dillo1))
+
+(define run-over-animal
+  (lambda (animal)
+    (cond
+      ((dillo? animal) (run-over-dillo animal))
+      ((parrot? animal) (run-over-parrot animal)))))
+
+;;; ÜBUNG animal-alive? Gibt an, ob ein Tier am Leben ist oder nicht
+(: animal-alive? (animal -> boolean))
+(check-expect (animal-alive? dillo1) #t)
+(check-expect (animal-alive? dillo2) #f)
+(check-expect (animal-alive? parrot1) #t)
+(check-expect (animal-alive? (make-parrot 2000 "")) #f)
+
+(define animal-alive?
+  (lambda (animal)
+    (cond
+      ((dillo? animal)  (dillo-alive? animal))
+      ((parrot? animal) (parrot-alive? animal)))))
+
+
 ; Eine leere Liste besteht aus... ? nix
 (define-record-functions empty-list
   make-empty-list
@@ -17,6 +130,21 @@
   cons?
   (first element)
   (rest (list-of element)))
+
+
+#|
+(define-record cons-of
+  (lambda (element)
+    cons
+    cons?
+    (first element)
+    (rest (list-of element))))
+
+(: cons (lambda (element) (element (list-of element) -> (cons-of element))))
+
+(: extract-list (lambda (element) ((element -> boolean) (list-of element) -> (list-of element))))
+|#
+
 
 ; Signaturvariable: fängt mit % an, kein Lambda notwendig
 (: cons (%element (list-of %element) -> (cons-of %element)))
