0f5c4aaf41445a043b6719e0bb2a8c77ef92bd8c
Mike Sperber <sperber@deinprogramm.de>
2020-04-23 11:00:00 +0200

    Update.

--- elixir_intro/lib/animal.ex
+++ elixir_intro/lib/animal.ex
@@ -4,6 +4,10 @@ defmodule Animal do
     @enforce_keys [:alive?, :weight]
     defstruct [:alive?, :weight]
     @type t :: %Dillo{alive?: boolean(), weight: number()}
+    @spec make(boolean(), number()) :: Dillo.t()
+    def make(alive?, weight) do
+      %Dillo{alive:? alive?, weight: weight}
+    end
     def foo() do
       %Dillo{alive?: 10, weight: "foo"}
     end
