bfbf1d317f1668e3ccb4a963c89b4cbb106b9e7b
Mike Sperber <sperber@deinprogramm.de>
2020-04-23 11:00:11 +0200

    Update.

--- elixir_intro/lib/animal.ex
+++ elixir_intro/lib/animal.ex
@@ -6,7 +6,7 @@ defmodule Animal do
     @type t :: %Dillo{alive?: boolean(), weight: number()}
     @spec make(boolean(), number()) :: Dillo.t()
     def make(alive?, weight) do
-      %Dillo{alive:? alive?, weight: weight}
+      %Dillo{alive?: alive?, weight: weight}
     end
     def foo() do
       %Dillo{alive?: 10, weight: "foo"}
