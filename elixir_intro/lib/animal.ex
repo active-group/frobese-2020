60d5709c1c2911357b5dca7e96711afb7b0d6987
Mike Sperber <sperber@deinprogramm.de>
2020-04-23 10:58:45 +0200

    Update.

--- elixir_intro/lib/animal.ex
+++ elixir_intro/lib/animal.ex
@@ -3,5 +3,9 @@ defmodule Animal do
     # 1 Typ zusammengesetzter Daten <-> 1 Modul
     @enforce_keys [:alive?, :weight]
     defstruct [:alive?, :weight]
+    @type t :: %Dillo{alive?: boolean(), weight: number()}
+    def foo() do
+      %Dillo{alive?: 10, weight: "foo"}
+    end
   end
 end
