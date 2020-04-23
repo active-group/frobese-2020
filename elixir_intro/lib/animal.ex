fd0597ddcb62a38a7fec85a7dbdf49befd90383c
Mike Sperber <sperber@deinprogramm.de>
2020-04-23 11:05:19 +0200

    Update.

--- elixir_intro/lib/animal.ex
+++ elixir_intro/lib/animal.ex
@@ -1,12 +1,13 @@
 defmodule Animal do
   defmodule Dillo do
     # 1 Typ zusammengesetzter Daten <-> 1 Modul
-    @enforce_keys [:alive?, :weight]
-    defstruct [:alive?, :weight]
-    @type t :: %Dillo{alive?: boolean(), weight: number()}
-    @spec make(boolean(), number()) :: Dillo.t()
-    def make(alive?, weight) do
-      %Dillo{alive?: alive?, weight: weight}
-    end
+    # @enforce_keys [:alive?, :weight]
+    # defstruct [:alive?, :weight]
+    # @type t :: %Dillo{alive?: boolean(), weight: number()}
+    # @spec make(boolean(), number()) :: Dillo.t()
+    # def make(alive?, weight) do
+    #   %Dillo{alive?: alive?, weight: weight}
+    # end
+    use QuickStruct, [alive?: boolean(), weight: number()]
   end
 end
