518d9ebcae8a55577b88ef0043f87da2ee598f37
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 13:25:17 +0200

    phone-mike, Anfang

---
+++ phone-mike/lib/phone.ex
@@ -0,0 +1,18 @@
+defmodule Phone do
+  @moduledoc """
+  Documentation for `Phone`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Phone.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
