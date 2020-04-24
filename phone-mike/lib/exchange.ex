22ea674ea50870c3e400eae70693aeddff95c653
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 14:16:01 +0200

    Update.

--- phone-mike/lib/exchange.ex
+++ phone-mike/lib/exchange.ex
@@ -69,4 +69,9 @@ defmodule Exchange do
     end
   end
 
+  @spec lookup_phone(pid(), term()):: :not_found | {:ok, pid()}
+  def lookup_phone(number) do
+    GenServer.call(exchange_pid, LookupPhone.make(number))
+  end
+
 end
