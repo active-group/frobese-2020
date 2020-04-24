988ee5c668c996f51bc76cd3f3987d48665c3673
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 13:38:44 +0200

    Update.

---
+++ phone-mike/lib/exchange.ex
@@ -0,0 +1,31 @@
+defmodule Exchange do
+  use GenServer
+
+  # Jedes Telefon hat eine Nummer
+  # => jede Connection hat eine Nummer
+  # Wollen Nachrichten austauschen zwischen den
+  # Connections => brauchen für jede Connection
+  # die Pid
+  # => benutzen dafür Process.register
+
+  # Jemand kauft ein Telefon, schließt es an,
+  # es nimmt Verbindung auf zum Exchange, der
+  # daraufhin einen Connection-Prozeß hochfährt.
+
+  # Blog-Post:
+  # init()
+  #
+  # def init [ip,port] do
+  #   {:ok,listen_socket} = :gen_tcp.listen(port,[:binary,{:packet, 0},{:active,true},{:ip,ip}])
+  #   {:ok,socket } = :gen_tcp.accept listen_socket
+  #   {:ok, %{ip: ip, port: port, socket: socket}}
+  # end
+
+  # wie @override in Java
+  @impl true
+  def ini({ip, port}) do
+  end
+
+
+end
+end
