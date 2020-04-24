45b755a814206505ffb1e1601cfdc3863b611eac
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 16:17:48 +0200

    Update.

--- phone-mike/lib/phone.ex
+++ phone-mike/lib/phone.ex
@@ -6,8 +6,19 @@ defmodule Phone do
     # connect redet mit accept
     {:ok, socket} = :gen_tcp.connect(ip, port, [:binary,
                                                 {:packet, :line},
+                                                # jedes Paket wird als Nachricht verschickt:
+                                                # {:tcp, socket, packet}
                                                 {:active, true}])
-    :gen_tcp.send(socket, inspect(number) <> "\n")
+    :ok = :gen_tcp.send(socket, inspect(number) <> "\n")
+    {:ok, socket}
+  end
+
+  # handle_cast verarbeitet *nur* GenServer.cast-Nachrichten
+  # handle_call dito nur GenServer.call
+
+  @impl true
+  def handle_info({:tcp, socket, line}, _socket) do
 
   end
+
 end
