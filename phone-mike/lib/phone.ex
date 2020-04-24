e69606a361ff095f926d6d4ad47b6be1d58bcd8a
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 16:13:03 +0200

    Update.

--- phone-mike/lib/phone.ex
+++ phone-mike/lib/phone.ex
@@ -2,11 +2,12 @@ defmodule Phone do
   use GenServer
 
   @impl true
-  def init({port, ip}) do
+  def init({port, ip, number}) do
     # connect redet mit accept
     {:ok, socket} = :gen_tcp.connect(ip, port, [:binary,
                                                 {:packet, :line},
                                                 {:active, true}])
+    :gen_tcp.send(socket, inspect(number) <> "\n")
 
   end
 end
