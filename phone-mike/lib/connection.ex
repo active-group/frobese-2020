a934cae0a4ce165b2f6b4931485a4e57419b4f58
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 14:24:24 +0200

    Update.

--- phone-mike/lib/connection.ex
+++ phone-mike/lib/connection.ex
@@ -1,3 +1,13 @@
 defmodule Connection do
+  use GenServer
 
+  def start_link(socket) do
+    GenServer.start_link(__MODULE__, socket,
+                        name: Exchange.phone_number_to_atom(number))
+  end
+
+  @impl true
+  def init(socket) do
+    # {:ok, ... state ...}
+  end
 end
