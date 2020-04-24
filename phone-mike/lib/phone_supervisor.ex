8622ff7171209af65f3c6e8ce3cda96d176e17eb
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 16:48:12 +0200

    Update.

---
+++ phone-mike/lib/phone_supervisor.ex
@@ -0,0 +1,20 @@
+defmodule PhoneSupervisor do
+  def children(ip, port) do
+    [
+      # Liste von Spezifikationen, wie Prozesse gestartet werden
+      %{
+        id: Phone123, # Pflicht
+        start: {Phone, :start_link, [ip, port, 123]}
+      },
+      %{
+        id: Phone456,
+        start: {Phone, :start_link, [ip, port, 456]}
+      }
+    ]
+  end
+
+  def start_link(ip, port) do
+    Supervisor.start_link(children(ip, port),
+                          strategy: :one_for_one)
+  end
+end
