1b17d56a5459f6506b991d4c58ae98653e687636
Mike Sperber <sperber@deinprogramm.de>
2020-04-24 10:06:46 +0200

    Update.

--- elixir_intro/lib/process_demo.ex
+++ elixir_intro/lib/process_demo.ex
@@ -32,6 +32,16 @@ defmodule ProcessDemo do
     end
   end
 
+  def inc(pid, i) do
+    send(pid, {:inc, i})
+  end
+
+  def get(pid) do
+    send(pid, {:get, self()})
+    receive do
+      msg -> msg
+    end
+  end
   def start_inc_loop(n) do
     pid = spawn(__MODULE__, :inc_loop, [n])
     Process.register(pid, :inc)
