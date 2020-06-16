ed57e77d8cbc499b178ba26bc8671e850901383d
Mike Sperber <sperber@deinprogramm.de>
2020-06-16 10:15:57 +0200

    Delete superfluous script.

--- erlbank/deployment/import_kibana_dashboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Imports dashboard
-# exported with curl -XGET 'localhost:5601/api/kibana/dashboards/export?dashboard=<dashboard-id>' > dashboards.json
-curl -XPOST localhost:5601/api/kibana/dashboards/import \
-    -H 'kbn-xsrf:true' \
-    -H 'Content-type:application/json' \
-    -d @dashboards.json
-
